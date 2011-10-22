79193bd26aacdb623ffa629d0b708627bf8c61f1
Raynos <raynos2@gmail.com>
2011-10-22 14:39:00 +0100

    moved src folders around and fixed build to be ordered

--- build.js
+++ build.js
@@ -4,9 +4,12 @@ var fs = require("fs"),
 process.chdir(__dirname);
 
 function makeFile(loc) {
+	return makeOrderedFile(fs.readdirSync(loc), loc);
+}
+
+function makeOrderedFile(files, loc) {
 	var code = ";(function (window, document, undefined) { \n";
 
-	var files = fs.readdirSync(loc);
 	files.forEach(function (filename) {
 		var file = fs.readFileSync(path.join(loc, filename));
 		code += file.toString() + "\n";
@@ -16,7 +19,19 @@ function makeFile(loc) {
 	return code;
 }
 
-fs.writeFileSync(path.join("lib", "DOM-shim.js"), makeFile(path.join("src")));
+fs.writeFileSync(path.join("lib", "DOM-shim.js"), makeOrderedFile(
+	[	
+		"Variables.js",
+		"Helpers.js",
+		"document.js",
+		"Node.js",
+		"bugs.js"
+	],
+	"src"
+));
 fs.writeFileSync(path.join("test", "tests.js"), makeFile(path.join("test", "test-suites")));
 fs.writeFileSync(path.join("test", "compliance.js"), makeFile(path.join("test", "compliance-suites")));
+
+
+
 console.log("done");
