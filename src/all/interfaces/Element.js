88a5f3b48d2b3e2b4e8ed58fa69658a2d8f3ff94
termi1uc1 <termi_uc@inbox.ru>
2012-01-26 16:59:30 +0400

    +! [wrong style] Implement HTML*Element.labels http://www.w3.org/TR/html5/forms.html#dom-lfe-labels

--- src/all/interfaces/Element.js
+++ src/all/interfaces/Element.js
@@ -6,7 +6,10 @@ module.exports = {
 	},
     classList: {
         get: getClassList
-    }
+    },
+	labels: {
+		get: getlabels
+	}
 }
 
 function getParentElement() {
@@ -37,4 +40,37 @@ function getClassList() {
         this._classList = dtlist;
         return dtlist;
     }
+}
+
+/*
+https://developer.mozilla.org/en/DOM/HTMLInputElement
+http://www.w3.org/TR/html5/forms.html#dom-lfe-labels
+require: HTMLLabelElement.control
+
+TODO:: fix wrong "labels" implimantation in Opera 11.6* https://twitter.com/statuses/162234890039988224
+*/
+function getlabels() {
+	if(!~["INPUT", "BUTTON", "KEYGEN", "METER", "OUTPUT", "PROGRESS", "TEXTAREA", "SELECT"].indexOf(this.nodeName))
+		return void 0;
+	
+	var node = this,
+		/**
+		 * represents the list of label elements, in [!]tree order[!]
+		 * @type {Array}
+		 */
+		result = this.id ?
+			Array.prototype.slice.apply(document.querySelectorAll("label[for='" + this.id + "']")) :	
+			[],
+		_lastInTreeOrder_index = result.length - 1;
+
+	while((node = node.parentNode) && (!node.control || node.control === this))
+		if(node.nodeName === "LABEL") {
+			
+			while(result[_lastInTreeOrder_index] && 
+				result[_lastInTreeOrder_index].compareDocumentPosition(node) & 2)//DOCUMENT_POSITION_PRECEDING
+				_lastInTreeOrder_index--;
+			result.splice(_lastInTreeOrder_index + 1, 0, node)
+		}
+		
+	return result;
 }
