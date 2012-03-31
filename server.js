7f5df83030d4b04967a59eccbd10c7d66a8c70c9
Raynos <raynos2@gmail.com>
2012-03-31 21:06:04 +0100

    updated server / package.json / test

--- server.js
+++ server.js
@@ -5,5 +5,7 @@ var browserify = require("browserify"),
 	fs = require("fs")
 
 http.createServer(function (req, res) {
+	var features = JSON.parse(url.parse(req.url, true).query.features)
 
+	console.log(features)
 }).listen(8084)
